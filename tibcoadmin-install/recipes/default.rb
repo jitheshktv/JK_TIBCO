2d238e223800a3708f3670c6bc6265be456b55c8
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-12 09:43:00 -0600

    Commented the rv_install and tra_install from the default recipe
    Commented the rv_install and tra_install from the default recipe which
    needs to be uncommented when testing the admin installation alone using
    test kitchen.

--- tibcoadmin-install/recipes/default.rb
+++ tibcoadmin-install/recipes/default.rb
@@ -9,6 +9,6 @@ include_recipe 'tibcoadmin-install::s3download'
 include_recipe 'tibcoadmin-install::responsefile'
 include_recipe 'java'
 # Uncomment the below line to run rv and tra install before admin when testing this recipe alone using kitchen
-include_recipe 'rv-install::default'
-include_recipe 'tra-install::default'
+# include_recipe 'rv-install::default'
+# include_recipe 'tra-install::default'
 include_recipe 'tibcoadmin-install::install'
