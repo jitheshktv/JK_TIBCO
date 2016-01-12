0ecce10894a59ed0a59cf4e0e37f121d873431db
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-11 22:22:14 -0600

    changed a directory name to use snake_case
    changed a directory name to use snake_case

--- tra-install/recipes/directories.rb
+++ tra-install/recipes/directories.rb
@@ -15,14 +15,14 @@ logs_instance_dir = node['tra-install']['install']['logs_instance_dir']
 tra_bin_target_path = node['tra-install']['install']['tra_bin_target_path']
 tra_home_dir = node['tra-install']['install']['tra_home_dir']
 tibco_cfgmgmt_dir = "#{tibco_install_dir}/tibco_cfg"
-tibco_thirdpartyDownload_dir = "#{tibco_install_dir}/thirdpartyDownload"
+tibco_thirdpartydownload_dir = "#{tibco_install_dir}/thirdpartyDownload"
 
 install_user = node['tra-install']['install']['user']
 install_group = node['tra-install']['install']['group']
 config_user = node['tra-install']['config']['user']
 config_group = node['tra-install']['config']['group']
 
-[tibco_home_dir, logs_home_dir, tibco_install_dir, tra_home_dir, tra_bin_target_path, tibco_cfgmgmt_dir, tibco_thirdpartyDownload_dir].each do |dir_name|
+[tibco_home_dir, logs_home_dir, tibco_install_dir, tra_home_dir, tra_bin_target_path, tibco_cfgmgmt_dir, tibco_thirdpartydownload_dir].each do |dir_name|
   directory dir_name do
     owner install_user
     group install_group
