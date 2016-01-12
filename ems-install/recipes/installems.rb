648336319741500a778db17e859379c1d0167a35
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-12 13:14:07 -0600

    corrected sudo
    corrected sudo

--- ems-install/recipes/installems.rb
+++ ems-install/recipes/installems.rb
@@ -17,7 +17,7 @@ ENV['JAVA_HOME'] = '/usr/lib/jvm/java'
 
 # Install ems binary using the response file
 execute 'installems' do
-  command "sudo -su #{user_name} #{ems_bin} -silent -V responseFile=#{ems_install_responsefile}"
+  command "sudo -u #{user_name} #{ems_bin} -silent -V responseFile=#{ems_install_responsefile}"
   not_if { File.exist? "#{tibemsd}" }
 end
 
