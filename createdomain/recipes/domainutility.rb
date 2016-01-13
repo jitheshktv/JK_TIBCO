058d1131e453c281950fe8d2c547dba2a882c7d2
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-12 22:32:08 -0600

    start hawkagent and admin

--- createdomain/recipes/domainutility.rb
+++ createdomain/recipes/domainutility.rb
@@ -13,13 +13,33 @@ tra_home_dir = node['createdomain-rv']['config']['tra_home_dir']
 domainutilitycmd = "#{tra_home_dir}/bin/domainutilitycmd"
 createdomain_cmdfile = node['createdomain-rv']['config']['createdomain_cmdfile']
 
-# Install the required yum packages for 32 bit
+domain_name = node['createdomain-rv']['config']['domain_name']
+tibcoadmin_domainhome_dir = node['createdomain-rv']['config']['tibcoadmin_domainhome_dir']
+tra_domainhome_dir = node['createdomain-rv']['config']['tra_domainhome_dir']
+hawkagent = "#{tra_domainhome_dir}/#{domain_name}/hawkagent_#{domain_name}"
+administrator = "#{tibcoadmin_domainhome_dir}/#{domain_name}/bin/tibcoadmin_#{domain_name}"
+
+# Install the required yum packages for running 32 bit domainutility on a 64 bit OS arch
 yum_package 'glibc.i686'
 yum_package 'libstdc++48.i686'
 
 # Execute the domain utility
 execute 'create_domain' do
   command "#{domainutilitycmd} --cmdFile #{createdomain_cmdfile}"
-  user install_user
-  group install_group
+  user config_user
+  group config_group
+end
+
+# Start the hawkagent
+execute 'start_hawkagent' do
+  command "nohup #{hawkagent} 2>&1 &"
+  user config_user
+  group config_group
+end
+
+# Start the admin domain
+execute 'start_admin' do
+  command "nohup #{administrator} 2>&1 &"
+  user config_user
+  group config_group
 end
