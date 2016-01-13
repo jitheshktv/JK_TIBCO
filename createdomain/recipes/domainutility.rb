d43ce9ce2e01c98159896939de79cb9df67794b3
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-12 22:56:50 -0600

    changed the cwd in hawkagent and admin startup

--- createdomain/recipes/domainutility.rb
+++ createdomain/recipes/domainutility.rb
@@ -16,8 +16,10 @@ createdomain_cmdfile = node['createdomain-rv']['config']['createdomain_cmdfile']
 domain_name = node['createdomain-rv']['config']['domain_name']
 tibcoadmin_domainhome_dir = node['createdomain-rv']['config']['tibcoadmin_domainhome_dir']
 tra_domainhome_dir = node['createdomain-rv']['config']['tra_domainhome_dir']
-hawkagent = "#{tra_domainhome_dir}/#{domain_name}/hawkagent_#{domain_name}"
-administrator = "#{tibcoadmin_domainhome_dir}/#{domain_name}/bin/tibcoadmin_#{domain_name}"
+tra_domain_dir = "#{tra_domainhome_dir}/#{domain_name}"
+hawkagent = "#{tra_domain_dir}/hawkagent_#{domain_name}"
+tibcoadmin_bin_dir = "#{tibcoadmin_domainhome_dir}/#{domain_name}/bin"
+administrator = "#{tibcoadmin_bin_dir}/tibcoadmin_#{domain_name}"
 
 # Install the required yum packages for running 32 bit domainutility on a 64 bit OS arch
 yum_package 'glibc.i686'
@@ -34,6 +36,7 @@ end
 # Start the hawkagent
 execute 'start_hawkagent' do
   command "nohup #{hawkagent} 2>&1 &"
+  cwd tra_domain_dir
   user config_user
   group config_group
 end
@@ -41,6 +44,7 @@ end
 # Start the admin domain
 execute 'start_admin' do
   command "nohup #{administrator} 2>&1 &"
+  cwd tibcoadmin_bin_dir
   user config_user
   group config_group
 end
