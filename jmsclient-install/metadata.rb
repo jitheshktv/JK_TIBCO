87f11c4935cfdc74b1da0635a0cfca13a16c3d8e
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-30 13:41:13 -0600

    correction in description

--- jmsclient-install/metadata.rb
+++ jmsclient-install/metadata.rb
@@ -2,6 +2,6 @@ name 'jmsclient-install'
 maintainer 'JitheshKumar (JK)'
 maintainer_email 'jiteshkumar.thekkeveettil@citi.com'
 license 'all_rights'
-description 'Add a machine to the EMS/DB based TIBCO admin domain'
+description 'Install JMS Client jars'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
