0af7b456c5385924262d1cd5efd0600ebb99bea0
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-01-30 13:42:37 -0600

    commenting the DB/EMS client installations
    there are new cookbooks for client installation

--- addmachine-dbems/recipes/default.rb
+++ addmachine-dbems/recipes/default.rb
@@ -6,6 +6,7 @@
 
 include_recipe 'addmachine-dbems::commandfilexml'
 include_recipe 'addmachine-dbems::tnsfile'
-include_recipe 'addmachine-dbems::installemsclient'
-include_recipe 'addmachine-dbems::installdbclient'
+# Commenting below as there is a new cookbook for client installation
+#include_recipe 'addmachine-dbems::installemsclient'
+#include_recipe 'addmachine-dbems::installdbclient'
 include_recipe 'addmachine-dbems::domainutility'
--- createdomain-dbems/recipes/default.rb
+++ createdomain-dbems/recipes/default.rb
@@ -6,6 +6,7 @@
 
 include_recipe 'createdomain-dbems::commandfilexml'
 include_recipe 'createdomain-dbems::tnsfile'
-include_recipe 'createdomain-dbems::installemsclient'
-include_recipe 'createdomain-dbems::installdbclient'
+# Commenting the below as there are seperate Cookbooks for those.
+#include_recipe 'createdomain-dbems::installemsclient'
+#include_recipe 'createdomain-dbems::installdbclient'
 include_recipe 'createdomain-dbems::domainutility'
