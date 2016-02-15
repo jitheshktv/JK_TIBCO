b4169bb624c9731fa8b31fc6972e52ad29bd34c0
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-02-15 13:41:58 -0600

    ports are calculated dynamically now

--- createdomain/recipes/commandfilexml.rb
+++ createdomain/recipes/commandfilexml.rb
@@ -8,8 +8,33 @@
 
 createdomain_cmdfile = node['createdomain-rv']['config']['createdomain_cmdfile']
 
+rv_port = node['createdomain-rv']['config']['rv_port']
+rv_network = node['createdomain-rv']['config']['rv_network']
+application_hawk_daemonport = rv_port + 74
+
+application_rv_daemon = "tcp:#{rv_port}"
+application_rv_network = rv_network
+application_rv_service = rv_port
+application_hawk_daemon = "tcp:#{application_hawk_daemonport}"
+application_hawk_network = rv_network
+application_hawk_service = application_hawk_daemonport
+tomcat_http_port = rv_port + 80
+tomcat_ajp_port = rv_port + 81
+tomcat_shutdown_port = rv_port + 82
+
 template 'CreateDomain.xml' do
   source 'CreateDomain.xml.erb'
   mode '0755'
   path createdomain_cmdfile
+  variables(
+    var_application_rv_daemon:  application_rv_daemon,
+    var_application_rv_network:  application_rv_network,
+    var_application_rv_service:  application_rv_service,
+    var_application_hawk_daemon:  application_hawk_daemon,
+    var_application_hawk_network:  application_hawk_network,
+    var_application_hawk_service:  application_hawk_service,
+    var_tomcat_http_port:  tomcat_http_port,
+    var_tomcat_ajp_port:  tomcat_ajp_port,
+    var_tomcat_shutdown_port:  tomcat_shutdown_port
+  )
 end
