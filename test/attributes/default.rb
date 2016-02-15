1a67e384edf7c249b7d2678e34f6766f5c221f5b
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-02-15 15:21:31 -0600

    test

--- test/attributes/default.rb
+++ test/attributes/default.rb
@@ -1,1 +1,4 @@
-default['rv-install']['install']['explicit_rvinstall_needed'] = 'true'
+default['test']['first_name'] = 'Jithesh'
+default['test']['last_name'] = 'TV'
+default['test']['city'] = 'Dallas'
+default['test']['x'] = 10
