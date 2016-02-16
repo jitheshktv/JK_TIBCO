6a866528e33570508a94ef15453fa812ff1ec6e8
MohamedAltaf <basheer.altaf@gmail.com>
2016-02-17 03:39:18 +0530

    Gold Settings Updated
    Updated

--- hawk-goldsettings/recipes/hawkagent_cfg.rb
+++ hawk-goldsettings/recipes/hawkagent_cfg.rb
@@ -17,7 +17,7 @@ end
 ruby_block 'Find and replce in hawkagent.cfg' do
 block do
 hawkagent_cfg = Chef::Util::FileEdit.new("#{hawkagent}")
-hawkagent_cfg.search_file_replace_line("-use_thread_pool", "-use_thread_pool=#{node['hawk-goldsettings']['hawkagent_cfg_config']['use_thread_pool']}")
+hawkagent_cfg.search_file_replace_line("-use_thread_pool", "-use_thread_pool #{node['hawk-goldsettings']['hawkagent_cfg_config']['use_thread_pool']}")
 not_if '-use_thread_pool'
 hawkagent_cfg.insert_line_if_no_match("-use_thread_pool", "-use_thread_pool=#{node['hawk-goldsettings']['hawkagent_cfg_config']['use_thread_pool']}")
 hawkagent_cfg.write_file
