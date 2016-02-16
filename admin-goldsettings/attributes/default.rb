68fe96d44f5e01ca4a8e2ba21560cb30fb44c91a
MohamedAltaf <basheer.altaf@gmail.com>
2016-02-17 03:15:33 +0530

    Gold Settings Updated
    Updated

--- admin-goldsettings/attributes/default.rb
+++ admin-goldsettings/attributes/default.rb
@@ -1,8 +1,8 @@
 default['admin-goldsettings']['config']['user'] = 'esbuser'
 default['admin-goldsettings']['config']['group'] = 'sgsup'
 
-default['admin-goldsettings']['config']['domain_name'] = 'RDS_DIT'
-default['admin-goldsettings']['config']['tibcoadmin_domainhome_dir'] = '/apps/tibco/domain/GTDIT/4.0/administrator/domain'
+default['admin-goldsettings']['config']['domain_name'] = 'TEST_DOMAIN'
+default['admin-goldsettings']['config']['tibcoadmin_domainhome_dir'] = '/apps/tibco/administrator/domain'
 
 default['admin-goldsettings']['tibcoadmin_tra_config']['heap_size_min'] = '1024M'
 default['admin-goldsettings']['tibcoadmin_tra_config']['heap_size_max'] = '2048M'
--- hawk-goldsettings/attributes/default.rb
+++ hawk-goldsettings/attributes/default.rb
@@ -1,8 +1,8 @@
 default['hawk-goldsettings']['config']['user'] = 'esbuser'
 default['hawk-goldsettings']['config']['group'] = 'sgsup'
 
-default['hawk-goldsettings']['config']['domain_name'] = 'RDS_DIT'
-default['hawk-goldsettings']['config']['tra_domainhome_dir'] = '/apps/tibco/domain/GTDIT/4.0/tra/domain'
+default['hawk-goldsettings']['config']['domain_name'] = 'TEST_DOMAIN'
+default['hawk-goldsettings']['config']['tra_domainhome_dir'] = '/apps/tibco/tra/domain'
 
 default['hawk-goldsettings']['hawkagent_cfg_config']['use_thread_pool'] = '256'
 
