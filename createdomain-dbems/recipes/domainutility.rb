ae06446ed7753f03babab711e393a9c335a30d7b
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-02-17 22:28:43 -0600

    correction

--- createdomain-dbems/recipes/domainutility.rb
+++ createdomain-dbems/recipes/domainutility.rb
@@ -16,6 +16,7 @@ tra_home_dir = "#{tibco_instance_dir}/tra/#{tra_version}"
 tra_bin = "#{tra_home_dir}/bin"
 createdomain_cmdfile = node['createdomain-dbems']['config']['createdomain_cmdfile']
 tra_domainhome_dir = "#{tibco_instance_dir}/tra/domain"
+domain_name = node['createdomain-dbems']['config']['domain_name']
 tra_domain_dir = "#{tra_domainhome_dir}/#{domain_name}"
 hawkagent = "#{tra_domain_dir}/hawkagent_#{domain_name}"
 
