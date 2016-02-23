b2d7c6a7dfa3e2f7009e0624d432853b6ee75afa
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-02-22 23:33:26 -0600

    new response file for 4.x version of hawk

--- hawk-install/recipes/responsefile.rb
+++ hawk-install/recipes/responsefile.rb
@@ -10,7 +10,15 @@
 hawk_bin_target_path = node['hawk-install']['install']['hawk_bin_target_path']
 hawk_install_responsefile = "#{hawk_bin_target_path}/#{node['hawk-install']['install']['response_file']}"
 
+hawk_major_version = node['hawk-install']['install']['hawk_major_version']
+
+if hawk_major_version >= '5.0'
+  response_file = 'TIBCOInstallResponsefile-hawk-5.x.silent.erb'
+else
+  response_file = 'TIBCOInstallResponsefile-hawk-4.x.silent.erb'
+end
+
 template hawk_install_responsefile do
-  source 'TIBCOInstallResponsefile-hawk.silent.erb'
+  source response_file
   mode '0755'
 end
