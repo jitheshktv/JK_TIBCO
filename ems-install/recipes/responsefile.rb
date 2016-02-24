75d9499e582e63876ac03e9f0cf52987704b2253
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-02-23 23:31:18 -0600

    reverted the changes to determine a new environment creation is needed or not

--- ems-install/recipes/responsefile.rb
+++ ems-install/recipes/responsefile.rb
@@ -9,28 +9,28 @@
 ems_bin_target_path = node['ems-install']['install']['ems_bin_target_path']
 ems_install_responsefile = "#{ems_bin_target_path}/#{node['ems-install']['install']['response_file']}"
 
-config_user = node['ems-install']['install']['user']
-config_user_home = "/home/#{config_user}"
-envinfo_dir = "#{config_user_home}/.TIBCOEnvInfo"
-envinfo_file = "#{envinfo_dir}/_envInfo.xml"
-tibco_home_name = node['ems-install']['install']['tibco_home_name']
-node.default[:createnewenvt] = nil
+# config_user = node['ems-install']['install']['user']
+# config_user_home = "/home/#{config_user}"
+# envinfo_dir = "#{config_user_home}/.TIBCOEnvInfo"
+# envinfo_file = "#{envinfo_dir}/_envInfo.xml"
+# tibco_home_name = node['ems-install']['install']['tibco_home_name']
+# node.default[:createnewenvt] = nil
 
-ruby_block 'check if TIBCOEnvInfo exists' do
-  block do
-    if File.readlines(envinfo_file).grep(/"#{tibco_home_name}"/).size > 0
-      node.set[:createnewenvt] = 'false'
-    else
-      node.set[:createnewenvt] = 'true'
-    end
-  end
-  only_if { File.exist?(envinfo_file) }
-end
+# ruby_block 'check if TIBCOEnvInfo exists' do
+#   block do
+#     if File.readlines(envinfo_file).grep(/"#{tibco_home_name}"/).size > 0
+#       node.set[:createnewenvt] = 'false'
+#     else
+#       node.set[:createnewenvt] = 'true'
+#     end
+#   end
+#   only_if { File.exist?(envinfo_file) }
+# end
 
 template ems_install_responsefile do
   source 'emsInstallResponsefile.silent.erb'
   mode '0755'
-  variables(
-    var_createnewenvt:  node[:createnewenvt]
-  )
+#   variables(
+#     var_createnewenvt:  node[:createnewenvt]
+#   )
 end
--- rv-install/recipes/responsefile.rb
+++ rv-install/recipes/responsefile.rb
@@ -10,28 +10,28 @@
 rv_bin_target_path = node['rv-install']['install']['rv_bin_target_path']
 rv_install_responsefile = "#{rv_bin_target_path}/#{node['rv-install']['install']['response_file']}"
 
-config_user = node['rv-install']['config']['user']
-config_user_home = "/home/#{config_user}"
-envinfo_dir = "#{config_user_home}/.TIBCOEnvInfo"
-envinfo_file = "#{envinfo_dir}/_envInfo.xml"
-tibco_home_name = node['rv-install']['install']['tibco_home_name']
-node.default[:createnewenvt] = nil
+# config_user = node['rv-install']['config']['user']
+# config_user_home = "/home/#{config_user}"
+# envinfo_dir = "#{config_user_home}/.TIBCOEnvInfo"
+# envinfo_file = "#{envinfo_dir}/_envInfo.xml"
+# tibco_home_name = node['rv-install']['install']['tibco_home_name']
+# node.default[:createnewenvt] = nil
 
-ruby_block 'check if TIBCOEnvInfo exists' do
-  block do
-    if File.readlines(envinfo_file).grep(/"#{tibco_home_name}"/).size > 0
-      node.set[:createnewenvt] = 'false'
-    else
-      node.set[:createnewenvt] = 'true'
-    end
-  end
-  only_if { File.exist?(envinfo_file) }
-end
+# ruby_block 'check if TIBCOEnvInfo exists' do
+#   block do
+#     if File.readlines(envinfo_file).grep(/"#{tibco_home_name}"/).size > 0
+#       node.set[:createnewenvt] = 'false'
+#     else
+#       node.set[:createnewenvt] = 'true'
+#     end
+#   end
+#   only_if { File.exist?(envinfo_file) }
+# end
 
 template rv_install_responsefile do
   source 'TIBCOInstallResponsefile-rv.silent.erb'
   mode '0755'
-  variables(
-    var_createnewenvt:  node[:createnewenvt]
-  )
+#   variables(
+#     var_createnewenvt:  node[:createnewenvt]
+#   )
 end
